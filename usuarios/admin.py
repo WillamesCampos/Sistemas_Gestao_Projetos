cc9d92edbf9453b36d1250ecd36ea784339d5bad
Willames <williames@arbache.com>
2022-03-29 16:26:53 -0300

    criando models de usuario, professor e aluno

---
+++ usuarios/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from usuarios.models import Aluno, Professor
+
+
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display=('codigo', 'email', 'nome')
+    search_fields=['codigo', 'email']
+
+
+class AlunoAdmin(admin.ModelAdmin):
+    list_display=('matricula', 'email', 'nome')
+    search_fields=['matricula', 'email']
+
+
+admin.site.register(Aluno, AlunoAdmin)
+admin.site.register(Professor, ProfessorAdmin)
---
+++ usuarios/views.py
@@ -0,0 +1,2 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Professor, Aluno
