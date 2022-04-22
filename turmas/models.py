512cfa417bbe1174382f3b66d046f3f3ddbb9e71
WillamesCampos <willwjccampos@gmail.com>
2022-04-22 17:42:34 -0300

    modelando turmas

---
+++ turmas/models.py
@@ -0,0 +1,22 @@
+from uuid import uuid4
+from django.db import models
+
+# Create your models here.
+class Turma(models.Model):
+    codigo = models.UUIDField(
+        primary_key=True,
+        default=uuid4,
+        editable=False
+    )
+    nome = models.CharField(
+        max_length=30
+    )
+    periodo = models.CharField(
+        max_length=6
+    )
+
+    class Meta:
+        db_table = 'tb_turma'
+
+    def __str__(self) -> str:
+        return f'{self.nome}'
